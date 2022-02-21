c3b8997425ea31271793343aafbe05e655b403dd
Yuri Zhulev <yuri.zhulyov@gmail.com>
2022-02-21 11:15:49 +0300

    feat: cards controller error handlers

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,23 +1,49 @@
-const Card = require('../models/card');
+const Card = require("../models/card");
+const { NotFoundError, ValidationError } = require("../Error/Errors");
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .populate('owner')
-    .then((cards) => res.send({ data: cards }));
+    .populate("owner")
+    .then((cards) => res.send({ data: cards }))
+    .catch((err) => {
+      res.status(500).send({ message: "На сервере произошла ошибка" });
+    });
 };
 
-module.exports.postCard = (req, res) => {
+module.exports.postCard = async (req, res) => {
   const { name, link } = req.body;
   const owner = req.user._id;
-  Card.create({ name, link, owner })
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: 'Error occured' }));
+
+  try {
+    if (!name || !link) {
+      throw new ValidationError("Введены неверные данные карточки.");
+    }
+    const card = await Card.create({ name, link, owner });
+    res.send(card);
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      res.send({ message: err.message });
+    } else {
+      res.status(500).send({ message: "На сервере произошла ошибка" });
+    }
+  }
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: 'Error occured' }));
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError("Карточка с указанным id не найдена");
+      }
+      res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err instanceof NotFoundError) {
+        res.send({ message: err.message });
+      } else {
+        res.status(500).send({ message: "На сервере произошла ошибка" });
+      }
+    });
 };
 
 module.exports.likeCard = (req, res) => {
@@ -30,14 +56,36 @@ module.exports.likeCard = (req, res) => {
     },
     { new: true }
   )
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: 'Error occured', err }));
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError("Карточка с указанным id не найдена");
+      }
+      res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err instanceof NotFoundError) {
+        res.send({ message: err.message });
+      } else {
+        res.status(500).send({ message: "На сервере произошла ошибка" });
+      }
+    });
 };
 
 module.exports.dislikeCard = (req, res) => {
   const cardId = req.params.cardId;
   const userId = req.user._id;
   Card.findByIdAndUpdate(cardId, { $pull: { likes: userId } }, { new: true })
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: 'Error occured', err }));
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError("Карточка с указанным id не найдена");
+      }
+      res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err instanceof NotFoundError) {
+        res.send({ message: err.message });
+      } else {
+        res.status(500).send({ message: "На сервере произошла ошибка" });
+      }
+    });
 };
