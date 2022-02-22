3b64409ff902a6f92492a343bc12ecec987b8f34
Yuri Zhulev <yuri.zhulyov@gmail.com>
2022-02-22 11:03:32 +0300

    fix: handle 404 page route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const {NotFoundError} = require('./Error/Errors')
 const { PORT = 3000 } = process.env;
 
 const app = express();
@@ -22,7 +23,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'))
-
+app.use('*', (req, res) => {
+  try {
+    throw new NotFoundError("Страница не найдена")
+  } catch (err) {
+    if (err instanceof NotFoundError) {
+      res.status(404).send({message: err.message})
+    }
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
